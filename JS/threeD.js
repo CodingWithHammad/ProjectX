15ac422089fced4e2176861cb9ba826bbb6d29c7
CodingWithHammad <codingwithhammad786@gmail.com>
2025-09-04 18:14:57 +0530

    working on htree js and enhance the code

---
+++ JS/threeD.js
@@ -0,0 +1,36 @@
+import * as THREE from "three"
+
+// Create a scene
+const scene = new THREE.Scene()
+scene.background = new THREE.Color("#F0F0F0")
+
+// Add a camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight , 0.1, 1000)
+camera.position.z = 5
+
+// Create a cube object
+const geo = new THREE.BoxGeometry()
+const material = new THREE.MeshLambertMaterial({color : "#468585"  , emissive : "#468585"})
+const cube = new THREE.Mesh(geo,material)
+scene.add(cube)
+
+// Add Lighting
+const light = new THREE.DirectionalLight(0x9CDBA6, 10)
+light.position.set(1,1,1)
+scene.add(light)
+
+// Set up renderer
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth , window.innerHeight)
+document.body.appendChild(renderer.domElement)
+
+// Animate scene
+function animate() {
+    requestAnimationFrame(animate);
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.06;
+    cube.rotation.z += 0.01;
+    renderer.render(scene,camera)
+}
+
+animate()
